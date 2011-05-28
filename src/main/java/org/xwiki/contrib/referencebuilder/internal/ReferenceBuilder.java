794c77ff69d652170f2c7ce761c3dcd19d455c5c
tmortagne <thomas.mortagne@gmail.com>
2011-05-28 11:41:13 +0200

    * added attachment reference resolver * added generic entity reference resolver

--- src/main/java/org/xwiki/contrib/referencebuilder/internal/ReferenceBuilder.java
+++ src/main/java/org/xwiki/contrib/referencebuilder/internal/ReferenceBuilder.java
@@ -2,9 +2,12 @@ package org.xwiki.contrib.referencebuilder.internal;
 
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.AttachmentReference;
+import org.xwiki.model.reference.AttachmentReferenceResolver;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
 import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
 
 public class ReferenceBuilder
 {
@@ -17,16 +20,16 @@ public class ReferenceBuilder
         this.componentManager = componentManager;
     }
 
-    public ReferenceBuilder get(String name, EntityType type)
+    public ReferenceBuilder get(String name, EntityType entityType)
     {
-        this.reference = new EntityReference(name, type, this.reference);
+        this.reference = new EntityReference(name, entityType, this.reference);
 
         return this;
     }
 
-    public ReferenceBuilder get(String name, String typeName)
+    public ReferenceBuilder get(String name, String entityTypeName)
     {
-        EntityType type = EntityType.valueOf(typeName.toUpperCase());
+        EntityType type = EntityType.valueOf(entityTypeName.toUpperCase());
 
         return get(name, type);
     }
@@ -47,6 +50,20 @@ public class ReferenceBuilder
         return this.reference;
     }
 
+    public EntityReference getReference(String resolverId, String entityTypeName, Object... parameters)
+    {
+        try {
+            EntityType entityType = EntityType.valueOf(entityTypeName);
+
+            EntityReferenceResolver<EntityReference> resolver =
+                this.componentManager.lookup(EntityReferenceResolver.class, resolverId + "/entity");
+
+            return resolver.resolve(this.reference, entityType, parameters);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public DocumentReference getDocumentReference()
     {
         return getDocumentReference("current");
@@ -61,6 +78,27 @@ public class ReferenceBuilder
         try {
             DocumentReferenceResolver<EntityReference> resolver =
                 this.componentManager.lookup(DocumentReferenceResolver.class, resolverId + "/entity");
+
+            return resolver.resolve(this.reference, parameters);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public AttachmentReference getAttachmentReference()
+    {
+        return getAttachmentReference("current");
+    }
+
+    public AttachmentReference getAttachmentReference(String resolverId, Object... parameters)
+    {
+        if (this.reference == null) {
+            return null;
+        }
+
+        try {
+            AttachmentReferenceResolver<EntityReference> resolver =
+                this.componentManager.lookup(AttachmentReferenceResolver.class, resolverId + "/entity");
 
             return resolver.resolve(this.reference, parameters);
         } catch (Exception e) {
